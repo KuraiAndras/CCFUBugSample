e700d1394f6224c909e00cffaae8675956e3fff9
Kurai András <k.andras9642@gmail.com>
2020-10-12 19:25:27 +0200

    Reference project as package

---
+++ CCFUBugSample.UnityProject/Assets/CCFUBugSample.UnityProject/Tester.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace CCFUBugSample.UnityProject
+{
+    public class Tester : MonoBehaviour
+    {
+        private void Start() => Debug.Log(new SampleService().TestSwitch(2));
+    }
+}
