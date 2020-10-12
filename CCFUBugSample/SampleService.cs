c9d8a9ec9bb8f9cffb29b7bb5b0a6c1359e9520b
Kurai András <k.andras9642@gmail.com>
2020-10-12 19:14:21 +0200

    Add shared project

---
+++ CCFUBugSample/SampleService.cs
@@ -0,0 +1,14 @@
+﻿namespace CCFUBugSample
+{
+    public sealed class SampleService
+    {
+        public string TestSwitch(int value) =>
+            value switch
+            {
+                1 => "One",
+                2 => "Two",
+                3 => "Three",
+                _ => "A lot",
+            };
+    }
+}
